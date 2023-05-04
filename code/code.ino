44f9dfcd7b03df4941b83fa278a3725069096b9c
Animated <chrisjnassif@gmail.com>
2023-05-04 09:48:46 -0400

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,14 +1,15 @@
 #define LED_RED 11
 #define LED_GRN 10
 #define LED_BLU 9
+#define SENSOR A0
 
 void setup() {
     pinMode(LED_RED, OUTPUT);
     pinMode(LED_GRN, OUTPUT);
     pinMode(LED_BLU, OUTPUT);
-    
+
 }
 
 void main() {
-
+  analogRead(SENSOR)
 }
