cffd18437b07db95281b931359328ea47a7f78dc
Aaron Thapa <notsmart98@gmail.com>
2023-05-04 09:33:50 -0400

    asdfgh

---
+++ code/code.ino
@@ -0,0 +1,14 @@
+#define LED_RED 11
+#define LED_GRN 10
+#define LED_BLU 9
+
+void setup() {
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_GRN, OUTPUT);
+    pinMode(LED_BLU, OUTPUT);
+    
+}
+
+void main() {
+
+}
